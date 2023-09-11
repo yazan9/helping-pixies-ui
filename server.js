7023099fca70e9fa3dae699c401eb9fb24e8ce98
yazan9 <yazan.khalaileh@gmail.com>
2023-09-11 14:38:56 -0700

    force https

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    } else {
+      next();
+    }
+  });
+
 app.use(express.static('./dist/helping-pixies-ui'));
 
 app.get('/*', (req, res) =>
