bc60a6a28d98096bb6cac89f520cfe351682e593
yazan9 <yazan.khalaileh@gmail.com>
2023-09-22 13:50:10 -0700

    add a PWA button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,16 +10,45 @@ import { Router } from '@angular/router';
 export class HomeComponent {
   constructor(private authService: AuthService, public router: Router){}
 
+  deferredPrompt: any;
+
   ngOnInit(): void {
     let isLoggedIn = this.authService.isLoggedIn.subscribe(loggedIn => {
-      let user = this.authService.getUser();
-      if(user?.user_type === 'provider'){
-        this.router.navigate(['/provider']);
-      }
-      else if(user?.user_type === 'client'){
-        this.router.navigate(['/dashboard']);
-      }
+      // let user = this.authService.getUser();
+      // if(user?.user_type === 'provider'){
+      //   this.router.navigate(['/provider']);
+      // }
+      // else if(user?.user_type === 'client'){
+      //   this.router.navigate(['/dashboard']);
+      // }
+      window.addEventListener('beforeinstallprompt', (e) => this.beforeInstallPrompt(e));
+
     });
-    
+  }
+
+  beforeInstallPrompt(e: any): void {
+    e.preventDefault();
+    this.deferredPrompt = e;
+    this.showInstallButton();
+  }
+
+  showInstallButton(): void {
+    const installButton = document.getElementById('install-button');
+    installButton!.style.display = 'block';
+    installButton!.addEventListener('click', () => this.installPWA());
+  }
+
+  installPWA(): void {
+    const installButton = document.getElementById('install-button');
+    installButton!.style.display = 'none';
+    this.deferredPrompt.prompt();
+    this.deferredPrompt.userChoice.then((choiceResult:any) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      this.deferredPrompt = null;
+    });
   }
 }
