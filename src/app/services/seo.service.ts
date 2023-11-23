71d80461d3d76d7e42544f7e4253318f1ff61f88
yazan9 <yazan.khalaileh@gmail.com>
2023-11-22 16:16:14 -0800

    pre beta release

--- src/app/services/seo.service.ts
+++ src/app/services/seo.service.ts
@@ -9,15 +9,15 @@ export class SeoService {
   constructor(private title: Title, private meta: Meta) {}
 
   setDefaultMeta() {
-    this.title.setTitle('Helping Pixies - House Cleaning Services in Victoria, BC');
+    this.title.setTitle('Helping Pixies - Connecting House Cleaners with Clients');
 
     let keywords = [
-    'cleaning services victoria bc',
-    'house cleaning services victoria bc',
-    'residential cleaning services victoria bc',
-    'home cleaning services victoria bc',
-    'home cleaning victoria bc',
-    'residential cleaning victoria bc'
+    'cleaning services',
+    'house cleaning services',
+    'residential cleaning services',
+    'home cleaning services',
+    'home cleaning',
+    'residential cleaning'
     ];
 
     const defaultDescription = 'A platform where house cleaners and clients can connect.';
